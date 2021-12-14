a10e18db2fe2d2b654d74328a41694d90a5b4b12
user <user@C1011>
2021-12-14 12:36:52 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication33/ConsoleApplication33.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double a, n;
+    cout << "Введите значение: " << endl;
+    cout << "n= ";
+    cin >> n;
+
+
+    cout << "an= " << ((1/pow(2,n))+(1/pow(3,n)));
+
+    system("pause");
+    return 0;
+}
